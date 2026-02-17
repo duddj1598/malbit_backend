45a0d79338d7b62a7fbc2a59cf66074842a84f77
juno <hellojuno6649@gmail.com>
2026-02-17 22:02:04 +0900

    start

---
+++ src/main/java/org/example/malbit_backend/MalbitBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.malbit_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalbitBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MalbitBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/malbit_backend/MalbitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.malbit_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalbitBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
