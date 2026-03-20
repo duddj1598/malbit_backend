aa7328e24cc5de77b14a49bca11583ff986b2f30
Baeseona <baeseona2930@gmail.com>
2026-03-21 03:12:40 +0900

    feat: add enums for disability type and cognitive level

---
+++ src/main/java/com/example/demo/domain/DisabilityType.java
@@ -0,0 +1,20 @@
+package com.example.demo.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum DisabilityType {
+    LANGUAGE("언어장애"),
+    CRANIAL_NERVE("뇌신경장애"),
+    HEARING("청각장애"),
+    ARTICULATION("조음장애"),
+    CONDUCTIVE_HEARING("전음성 난청"),
+    SENSORINEURAL_HEARING("감음신경성 난청"),
+    FUNCTIONAL_VOICE("기능성 발성장애"),
+    LARYNGEAL("후두장애"),
+    ORAL("구강장애");
+
+    private final String description;
+}
